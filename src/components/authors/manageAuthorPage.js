add72e44d62dad01828bfed682f40f9c388d47e0
nghiango <nghiangodinh@yahoo.com>
2016-11-18 14:47:47 +0700

    add configuration settings files

--- src/components/authors/manageAuthorPage.js
+++ src/components/authors/manageAuthorPage.js
@@ -6,83 +6,87 @@ var AuthorForm = require('./authorForm');
 var AuthorApi = require('../../api/authorApi');
 var toastr = require('toastr');
 
+////////////////// ////////////////////
+
 var ManageAuthorPage = React.createClass({
-	mixins: [
-		Router.Navigation
-	],
+    mixins: [Router.Navigation],
 
-	statics: {
-		willTransitionFrom: function(transition, component) {
-			if (component.state.dirty && !confirm('Leave without saving?')) {
-				transition.abort();
-			}
-		}
-	},
+    statics: {
+        willTransitionFrom: function (transition, component) {
+            if (component.state.dirty && !confirm('Leave without saving?')) {
+                transition.abort();
+            }
+        }
+    },
 
-	getInitialState: function() {
-		return {
-			author: { id: '', firstName: '', lastName: '' },
-			errors: {},
-			dirty: false
-		};
-	},
+    getInitialState: function () {
+        return {
+            author: {
+                id: '',
+                firstName: '',
+                lastName: ''
+            },
+            errors: {},
+            dirty: false
+        };
+    },
 
-	componentWillMount: function() {
-		var authorId = this.props.params.id; //from the path '/author:id'
+    componentWillMount: function () {
+        var authorId = this.props.params.id; //from the path '/author:id'
 
-		if (authorId) {
-			this.setState({author: AuthorApi.getAuthorById(authorId) });
-		}
-	},
+        if (authorId) {
+            this.setState({
+                author: AuthorApi.getAuthorById(authorId)
+            });
+        }
+    },
 
-	setAuthorState: function(event) {
-		this.setState({dirty: true});
-		var field = event.target.name;
-		var value = event.target.value;
-		this.state.author[field] = value;
-		return this.setState({author: this.state.author});
-	},
+    setAuthorState: function (event) {
+        this.setState({dirty: true});
+        var field = event.target.name;
+        var value = event.target.value;
+        this.state.author[field] = value;
+        return this.setState({author: this.state.author});
+    },
 
-	authorFormIsValid: function() {
-		var formIsValid = true;
-		this.state.errors = {}; //clear any previous errors.
+    authorFormIsValid: function () {
+        var formIsValid = true;
+        this.state.errors = {}; //clear any previous errors.
 
-		if (this.state.author.firstName.length < 3) {
-			this.state.errors.firstName = 'First name must be at least 3 characters.';
-			formIsValid = false;
-		}
+        if (this.state.author.firstName.length < 3) {
+            this.state.errors.firstName = 'First name must be at least 3 characters.';
+            formIsValid = false;
+        }
 
-		if (this.state.author.lastName.length < 3) {
-			this.state.errors.lastName = 'Last name must be at least 3 characters.';
-			formIsValid = false;
-		}
+        if (this.state.author.lastName.length < 3) {
+            this.state.errors.lastName = 'Last name must be at least 3 characters.';
+            formIsValid = false;
+        }
 
-		this.setState({errors: this.state.errors});
-		return formIsValid;
-	},
+        this.setState({errors: this.state.errors});
+        return formIsValid;
+    },
 
-	saveAuthor: function(event) {
-		event.preventDefault();
+    saveAuthor: function (event) {
+        event.preventDefault();
 
-		if (!this.authorFormIsValid()) {
-			return;
-		}
+        if (!this.authorFormIsValid()) {
+            return;
+        }
 
-		AuthorApi.saveAuthor(this.state.author);
-		this.setState({dirty: false});
-		toastr.success('Author saved.');
-		this.transitionTo('authors');
-	},
+        AuthorApi.saveAuthor(this.state.author);
+        this.setState({dirty: false});
+        toastr.success('Author saved.');
+        this.transitionTo('authors');
+    },
 
-	render: function() {
-		return (
-			<AuthorForm
-				author={this.state.author}
-				onChange={this.setAuthorState}
-				onSave={this.saveAuthor}
-				errors={this.state.errors} />
-		);
-	}
+    render: function () {
+        return (<AuthorForm
+            author={this.state.author}
+            onChange={this.setAuthorState}
+            onSave={this.saveAuthor}
+            errors={this.state.errors}/>);
+    }
 });
 
 module.exports = ManageAuthorPage;
